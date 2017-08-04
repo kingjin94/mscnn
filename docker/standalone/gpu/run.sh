a93c981656ccad9c2b98b3bf32fa1f5e6296da58
matthias <matthias.mayer@tum.de>
2017-08-04 15:12:17 +0200

    [Add] A example of how to call the dockerimage

---
+++ docker/standalone/gpu/run.sh
@@ -0,0 +1,1 @@
+sudo nvidia-docker run -ti -v /media/matthias/Programme1/Dokumente/BachelorArbeit/KIT_CarDetect/:/home/data/ -v /home/matthias/mscnn-master/examples/:/home/mscnn/examples/ caffe:simplyPython
