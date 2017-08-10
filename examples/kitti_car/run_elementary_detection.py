c26568350fae343b9cc92b6ad4223c7fd0acb761
matthias <matthias.mayer@tum.de>
2017-08-10 17:51:09 +0200

    [Change] Elem_det can now be called with args too (folder of model & model name)

--- examples/kitti_car/run_elementary_detection.py
+++ examples/kitti_car/run_elementary_detection.py
@@ -1,32 +1,63 @@
+# Evaluation script for MSCNN models
+# To be run  in /examples/kitti_car/
+# specify Name(!) of dir with .caffemodel to be evaluated
+# specify which caffemodel to load
+
+import os as os #paths
+import sys
+
+if not len(sys.argv) == 3:
+    print 'Specify modelDir and model to evaluate'
+    quit()
+
+modelDir = os.path.abspath(sys.argv[1])
+modelName = sys.argv[1]
+if not os.path.exists(modelDir):
+    print 'Specify valid model Dir'
+    quit()
+
+model = sys.argv[2]
+
+print '------------------------------------------------------------------'
+print 'Called with: '
+print modelDir
+print model
+print '------------------------------------------------------------------'
+print ''
+
+
 import numpy as np
 import numpy.matlib
-import os as os #paths
 import re #regex
 import scipy.misc #imresize...
 
-# Get Caffe ready
+# Set up model to be evaluated
+model_weights = os.path.join(modelDir, model)#'mscnn_kitti_train_2nd_iter_25000.caffemodel')
+assert os.path.exists(model_weights)
+model_definition = os.path.join(modelDir, 'mscnn_deploy.prototxt')
+assert os.path.exists(model_definition)
+
+# Init Caffe
+os.environ['GLOG_minloglevel'] = '2'
 caffe_root = '/home/mscnn/'
 import sys
 sys.path.insert(0, caffe_root + 'python')
 import caffe
-
-# Set up model to be evaluated
-model = 'mscnn-7s-384'
-print 'Evaluating model in ' + model
-root_dir = os.path.join(caffe_root, 'examples/kitti_car/' + model + '/')
-model_weights = os.path.join(root_dir, 'mscnn_kitti_train_2nd_iter_25000.caffemodel')
-assert os.path.exists(model_weights)
-model_definition = os.path.join(root_dir, 'mscnn_deploy.prototxt')
-assert os.path.exists(model_definition)
-imgW = 1280; imgH = 384; # Size of pictures while training
-
-# Init caffe
 caffe.set_mode_gpu()
 caffe.set_device(0) #Which gpu if there are multiple
 net = caffe.Net(model_definition, model_weights, caffe.TEST)
 
+# Global Vars
+mu = np.array([104,117,123]) # Color offset
+imgW = 1280; imgH = 384; # Size of pictures while training
+#modelDir = 'mscnn-7s-384'
+outputDir = '/tmp/detections_' +modelName+'_'+model+'_'+ str(time.strftime("%H_%M_%S")) + '/'
+# Set up place to store results
+if not os.path.exists(outputDir):
+    os.makedirs(outputDir)
+
 # Set up data
-data_root = '/home/data/'
+data_root = '/home/data/KITTI/'
 image_dir = os.path.join(data_root, 'training/image_2/')
 assert os.path.exists(image_dir)
 comp_id = 'test'
@@ -34,13 +65,6 @@ image_list = os.listdir(image_dir)
 only_png = re.compile(r'.png')
 image_list = filter(only_png.search, image_list) # List of all images
 nImg = len(image_list)
-
-# Set up place to store results
-if not os.path.exists('/home/detections_' + model +'/'):
-    os.makedirs('/home/detections_' + model +'/')
-
-# Set up filters - normalize color
-mu = np.array([104,117,123])
 
 # Loop over all pictures
 for k in range(0, nImg):
@@ -69,9 +93,11 @@ for k in range(0, nImg):
     # Save for processing in matlab
     id_filter = re.compile(r"[0-9]")
     ID = filter(id_filter.search, image_list[k]) #Find Img ID with regex
-    
-    np.savetxt('/home/detections_' + model +'/bbox_preds_'+str(ID)+'.txt', bbox_preds)
-    np.savetxt('/home/detections_' + model +'/cls_pred_'+str(ID)+'.txt', cls_pred)
-    np.savetxt('/home/detections_' + model +'/tmp_'+str(ID)+'.txt', tmp)
 
-print 'Results saved to /home/detections_' + model +'/'
+    np.savetxt(outputDir+'bbox_preds_'+str(ID)+'.txt', bbox_preds)
+    np.savetxt(outputDir+'cls_pred_'+str(ID)+'.txt', cls_pred)
+    np.savetxt(outputDir+'tmp_'+str(ID)+'.txt', tmp)
+
+os.system('tar czf /home/output/detections_' + str(modelName) + '_' + str(model) + '.tar.gz ' + str(outputDir))
+os.system('rm -rf ' + str(outputDir))
+print 'Done, all detections saved to: /home/detections_' + str(modelName) + '_' + str(model) + '.tar.gz '
