4fc9cd0cb8ec39c191e33dcaca43649722323aff
matthias <matthias.mayer@tum.de>
2017-08-28 17:41:16 +0200

    [Exp] incremental learning with lateral extension, some learning turned off

---
+++ examples/kitti_car/mscnn-7s-384-incremental_learning_lat_aug_0125width/train_mscnn.sh
@@ -0,0 +1,19 @@
+GLOG_logtostderr=1 ../../../build/tools/caffe train \
+  --solver=solver_1_1st.prototxt \
+  --weights=../../../models/VGG/VGG_ILSVRC_16_layers.caffemodel \
+  --gpu=0  2>&1 | tee log_1_1st.txt
+
+GLOG_logtostderr=1 ../../../build/tools/caffe train \
+  --solver=solver_1_2st.prototxt \
+  --weights=mscnn_kitti_train_1_1st_iter_4000.caffemodel \
+  --gpu=0  2>&1 | tee log_1_2st.txt
+
+GLOG_logtostderr=1 ../../../build/tools/caffe train \
+  --solver=solver_2_1nd.prototxt \
+  --weights=mscnn_kitti_train_1_2st_iter_6000.caffemodel \
+  --gpu=0  2>&1 | tee log_2_1nd.txt
+
+GLOG_logtostderr=1 ../../../build/tools/caffe train \
+  --solver=solver_2_2nd.prototxt \
+  --weights=mscnn_kitti_train_2_1nd_iter_10000.caffemodel \
+  --gpu=0  2>&1 | tee log_2_2nd.txt
